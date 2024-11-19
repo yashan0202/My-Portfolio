510f48b37a8b95d7fee4b51425f8421ab6f22927
Yash <yashuan0202@gmail.com>
2024-11-19 16:11:01 +0530

    Update LandingSection.js

---
+++ portfolio/src/components/LandingSection.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./LandingSection.css";
+
+const LandingSection = () => {
+  return (
+    <section id="landing" className="landing">
+      <img src="/path-to-avatar.jpg" alt="Avatar" className="avatar" />
+      <h1>Your Name</h1>
+      <p>A short bio about your experience and interests.</p>
+    </section>
+  );
+};
+
+export default LandingSection;
